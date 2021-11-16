2397c69dfd434453001d8d8928afb873ac2e953c
Henry Sloan <henryksloan@gmail.com>
2021-11-16 15:18:57 -0500

    Fix WRAM addressing in DMG mode

--- src/bus.cpp
+++ src/bus.cpp
@@ -1,7 +1,5 @@
 #include "bus.h"
 
-#include <utility>
-
 void Bus::tick(int cpu_tcycles) {
   int cpu_multiplier = double_speed ? 2 : 1;
   int dma_ticks = progressDma();
@@ -66,7 +64,7 @@ uint8_t Bus::read(uint16_t addr) {
   } else if (addr >= 0xC000 && addr < 0xD000) {
     return wram[addr - 0xC000];
   } else if (addr >= 0xD000 && addr < 0xE000) {
-    size_t bank = 0x1000 * (cgb_mode ? wram_bank : 0);
+    size_t bank = 0x1000 * (cgb_mode ? wram_bank : 1);
     return wram[bank + (addr - 0xD000)];
   } else if (addr >= 0xE000 && addr < 0xFE00) {
     // TODO: Echo RAM
@@ -91,7 +89,7 @@ void Bus::write(uint16_t addr, uint8_t data) {
   } else if (addr >= 0xC000 && addr < 0xD000) {
     wram[addr - 0xC000] = data;
   } else if (addr >= 0xD000 && addr < 0xE000) {
-    size_t bank = 0x1000 * (cgb_mode ? wram_bank : 0);
+    size_t bank = 0x1000 * (cgb_mode ? wram_bank : 1);
     wram[bank + (addr - 0xD000)] = data;
   } else if (addr >= 0xE000 && addr < 0xFE00) {
     // TODO: Echo RAM
